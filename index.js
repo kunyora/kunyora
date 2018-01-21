6e9e4cea0817d61478457650b031d41cb8870f8e
badewa kayode <badewakayode1@gmail.com>
2018-01-21 09:57:41 +0100

    Add Window access to Composer-client for the browser

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ let c = null;
 
 try{
 	if (window){
-		c = require("./dist/composer-client-frontend")
+		c = require("./dist/composer-client-frontend");
+		//make it accessible globally through the window
+		window.composer = c
 	}
 }catch (err){
 	c = require("./dist/composer-client-backend")
