13188590d306503699206279113218d690aa0fc9
badewa kayode <badewakayode1@gmail.com>
2018-02-17 17:25:26 +0100

    Add Comments

--- src/events/index.js
+++ src/events/index.js
@@ -1,16 +1,30 @@
+/**
+ * @description create a new event emitter
+ * @return {*}
+ * @constructor
+ */
 export default function Emitter() {
 
 	const _events = {};
 
 	return {
-		//event listener
+		/**
+		 * @description creates an event listener
+		 * @param eventName the name of the event
+		 * @param callback the function to be called
+		 */
 		on(eventName, callback) {
 			const event = _events;
 			event[eventName] = event[eventName] || [];
 			event[eventName].push(callback);
 		},
 
-//fire the event
+		/**
+		 * @description emits an event
+		 * @param eventName the event name
+		 * @param args the argument to pass the the listeners callback function
+		 * @return {boolean}
+		 */
 		emit(eventName, ...args) {
 			const event = _events;
 			if (!(eventName in event)) return false;
