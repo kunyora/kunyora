8263f293ab9e288b4b0a215e3a843ca5087e6f4e
gbenga504 <daveanifowoshe@ymail.com>
2018-04-01 21:43:00 +0100

    Add comments

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,3 +1,21 @@
+/**
+ * This simple function controls the middlewares of the application 
+ * This contains the logic of [useBeforeRequest] and [useAfterResponse] helper functions 
+ * which help in running operations before a request is handled and after a response is gotten respectively 
+ * 
+ * Its used in this manner 
+ * ```
+ *    clientInstance.middleware({useBeforeRequest: function(){
+ *        // do something like setting authorization headers 
+ *    }, useAfterReponse: function(response){
+ *        // do something with response 
+ *    }})
+ * 
+ * ```
+ * 
+ * @return {Object}
+ * @param {Object} config
+ */
 export default config => ({
   middleware: function({ useBeforeRequest, useAfterResponse } = {}) {
     if (useBeforeRequest) {
--- src/option.js
+++ src/option.js
@@ -1,50 +1,89 @@
-import invariant from "invariant"
+import invariant from "invariant";
 
 const defaultConfig = {
-	nouns: [],
-	thenables: {},
-	catchables: {},
-	headers:{}
+  nouns: [],
+  thenables: {},
+  catchables: {},
+  headers: {}
 };
 
+/**
+ * This function re-formats the config object supplied by the user by concatenating the object with a default config
+ * used by the application
+ * It returns an object containing the reformatted config along with the nouns generated from the url configs supplied by the user
+ *
+ * @return {Object}
+ * @param {object} config
+ *
+ */
 export default function Option(config) {
-	let option = Object.assign({}, defaultConfig, config);
-	option.nouns = generateNouns(option);
-	//validate thenables and catchables
-
-
-	return option
-}
-
-function formatSlash(name) {
-	return name.replace(/\/+(\w)/g,(m,p1)=>p1.toUpperCase())
+  let option = Object.assign({}, defaultConfig, config);
+  option.nouns = generateNouns(option);
+  //@Todo validate thenables and catchables
+  return option;
 }
 
 /**
+ * This simple function formats the path supplied by the user in the config such that it appears as if its camelCased
+ * For instance, path: admin/login becomes adminLogin.
+ * removes all alphabets that comes after a forward slash and replaces them by a camelCased representation of the alphabet itself
  *
+ * @return {string}
+ * @param {string} name
+ */
+function formatSlash(name) {
+  return name.replace(/\/+(\w)/g, (m, p1) => p1.toUpperCase());
+}
+
+/**
+ * This function generates a standard noun that the application uses internally
+ * The standard noun basically has the structure {path: value, name: value}
+ * It generate the standard noun by also formatting the name property using the [formatSlash] helper function
+ *
+ * @return {Array}
  * @param nouns {string[]|object[]}
  */
-function generateNouns({nouns = []}) {
-	return nouns.map((noun, index) => {
-		if (typeof noun === "string") {
-			validateNounString(noun);
-			return {path: noun, name: formatSlash(noun)}
-		} else if (typeof noun === "object" && noun !== null) {
-			validateNoun(noun);
-			return {path:noun.path,name:formatSlash(noun.name)}
-		} else {
-			throw Error("Error: all nouns should either be a string or an object")
-		}
-	})
+function generateNouns({ nouns = [] }) {
+  return nouns.map((noun, index) => {
+    if (typeof noun === "string") {
+      validateNounString(noun);
+      return { path: noun, name: formatSlash(noun) };
+    } else if (typeof noun === "object" && noun !== null) {
+      validateNounObject(noun);
+      return { path: noun.path, name: formatSlash(noun.name) };
+    } else {
+      throw Error("Error: all nouns should either be a string or an object");
+    }
+  });
 }
 
-function validateNounString(str, v) {
-	invariant(typeof str === "string", "Error: " + (!!v) ? `property ${v} in noun not a string` : `noun passed is not a string`);
-	//
-	invariant(/^[0-9a-zA-Z_/]+$/.test(str), "Error: " + (!!v) ? `property ${v} in noun contains invalid char. alphanumeric and underscore allowed` : ` noun contains invalid char. alphanumeric and underscore allowed`)
+/**
+ * This function simply validates the nouns which are of type string
+ *
+ * @param {string} str
+ * @param {string} type
+ */
+function validateNounString(str, type) {
+  invariant(
+    typeof str === "string",
+    "Error: " + !!type
+      ? `property ${type} in noun not a string`
+      : `noun passed is not a string`
+  );
+  invariant(
+    /^[0-9a-zA-Z_/]+$/.test(str),
+    "Error: " + !!type
+      ? `property ${type} in noun contains invalid char. alphanumeric and underscore allowed`
+      : ` noun contains invalid char. alphanumeric and underscore allowed`
+  );
 }
 
-function validateNoun({path, name}) {
-	validateNounString(path, "path");
-	validateNounString(name, "name")
+/**
+ * This function validates nouns of type object by using validateNounString internally
+ *
+ * @param {object{string, string}} param0
+ */
+function validateNounObject({ path, name }) {
+  validateNounString(path, "path");
+  validateNounString(name, "name");
 }
